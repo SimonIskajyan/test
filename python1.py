4a4b816809ad055df505dae9f0198ca95070f681
SimonIskajyan <82520870+SimonIskajyan@users.noreply.github.com>
2021-04-15 13:43:20 +0400

    homework

---
+++ python1.py
@@ -0,0 +1,28 @@
+s = 80 #km
+t = 1 #hour
+v = s / t
+print (v)
+
+P = 1000
+i = 0.15
+t = 365
+T = 100
+n = T/t
+S = P * (1 + i * n)
+print (S)
+
+p = 3.14
+r = 10
+
+areaofcircle = p * r ** 2
+print (areaofcircle)
+
+a = 115
+b = 4
+c = (a + b) ** 2
+print (c)
+
+a = 11
+b = 10
+a = b
+print (b)
